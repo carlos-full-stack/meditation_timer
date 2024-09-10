2dec379873b00671fec73beab220d2664e45b7d6
Chally <carlosgilino@gmail.com>
2024-09-10 09:54:50 +0200

    fix start button

--- js/main.js
+++ js/main.js
@@ -1,14 +1,11 @@
 let intervalId;
 let remainingTime;
 let pausedTime;
-let bellStart;
-let bellFinish;
-let bellStartFinish;
-let silence;
 let currentPage = 'timer';
 let music;
 let songtitle;
 let bellSound;
+let bellTime = 'start';
 
 document.addEventListener('DOMContentLoaded', () => {
     const startPauseButton = document.querySelector('.control__play-pause');
@@ -68,7 +65,8 @@ function startTimer() {
 
         if (!pausedTime) {
 
-            if (!silence && !bellFinish) { // Play the bell if is not silence or played at the end
+
+            if (bellTime !== 'silence' && bellTime !== 'finish') { // Play the bell if is not silence or played at the end
 
                 playBell();
             }
@@ -86,7 +84,7 @@ function startTimer() {
 
                 updateDisplay(remainingTime);
 
-                if (bellFinish || bellStartFinish) {
+                if (bellTime === 'finish' || bellTime == 'startFinish') {
 
                     playBell();
                 }
@@ -150,13 +148,7 @@ function stopBell() {
         bellSound.currentTime = 0;
     }
 }
-function updateBellOptions(option) {
-    bellStart = (option === 'start');
-    bellFinish = (option === 'finish');
-    bellStartFinish = (option === 'both');
-    silence = (option === 'silence');
 
-}
 function showBellConfig(event) {
 
     if (currentPage !== 'bellConfig') {
@@ -185,7 +177,7 @@ function showBellConfig(event) {
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
                     <path d="M208 16c0-8.8 7.2-16 16-16s16 7.2 16 16l0 16.8c80.9 8 144 76.2 144 159.2l0 29.1c0 43.7 17.4 85.6 48.3 116.6l2.8 2.8c8.3 8.3 13 19.6 13 31.3c0 24.5-19.8 44.3-44.3 44.3L44.3 416C19.8 416 0 396.2 0 371.7c0-11.7 4.7-23 13-31.3l2.8-2.8C46.6 306.7 64 264.8 64 221.1L64 192c0-83 63.1-151.2 144-159.2L208 16zm16 48C153.3 64 96 121.3 96 192l0 29.1c0 52.2-20.7 102.3-57.7 139.2L35.6 363c-2.3 2.3-3.6 5.4-3.6 8.7c0 6.8 5.5 12.3 12.3 12.3l359.4 0c6.8 0 12.3-5.5 12.3-12.3c0-3.3-1.3-6.4-3.6-8.7l-2.8-2.8c-36.9-36.9-57.7-87-57.7-139.2l0-29.1c0-70.7-57.3-128-128-128zM193.8 458.7c4.4 12.4 16.3 21.3 30.2 21.3s25.8-8.9 30.2-21.3c2.9-8.3 12.1-12.7 20.4-9.8s12.7 12.1 9.8 20.4C275.6 494.2 251.9 512 224 512s-51.6-17.8-60.4-42.7c-2.9-8.3 1.4-17.5 9.8-20.4s17.5 1.4 20.4 9.8z"/>
                 </svg>
-                <input type="radio" name="options" id="start" checked>
+                <input type="radio" name="options" id="start">
                 <span class="option-label">Start</span>
             </label>
         </li>
@@ -200,13 +192,13 @@ function showBellConfig(event) {
             </label>
         </li>
         <li>
-            <label for="both" class="option-container">
+            <label for="startFinish" class="option-container">
                 <i class="fa-solid fa-hourglass-start"></i>
                 <i class="fa-solid fa-hourglass-end"></i>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
                     <path d="M208 16c0-8.8 7.2-16 16-16s16 7.2 16 16l0 16.8c80.9 8 144 76.2 144 159.2l0 29.1c0 43.7 17.4 85.6 48.3 116.6l2.8 2.8c8.3 8.3 13 19.6 13 31.3c0 24.5-19.8 44.3-44.3 44.3L44.3 416C19.8 416 0 396.2 0 371.7c0-11.7 4.7-23 13-31.3l2.8-2.8C46.6 306.7 64 264.8 64 221.1L64 192c0-83 63.1-151.2 144-159.2L208 16zm16 48C153.3 64 96 121.3 96 192l0 29.1c0 52.2-20.7 102.3-57.7 139.2L35.6 363c-2.3 2.3-3.6 5.4-3.6 8.7c0 6.8 5.5 12.3 12.3 12.3l359.4 0c6.8 0 12.3-5.5 12.3-12.3c0-3.3-1.3-6.4-3.6-8.7l-2.8-2.8c-36.9-36.9-57.7-87-57.7-139.2l0-29.1c0-70.7-57.3-128-128-128zM193.8 458.7c4.4 12.4 16.3 21.3 30.2 21.3s25.8-8.9 30.2-21.3c2.9-8.3 12.1-12.7 20.4-9.8s12.7 12.1 9.8 20.4C275.6 494.2 251.9 512 224 512s-51.6-17.8-60.4-42.7c-2.9-8.3 1.4-17.5 9.8-20.4s17.5 1.4 20.4 9.8z"/>
                 </svg>
-                <input type="radio" name="options" id="both">
+                <input type="radio" name="options" id="startFinish">
                 <span class="option-label">Start and end</span>
             </label>
         </li>
@@ -223,61 +215,30 @@ function showBellConfig(event) {
     </ul>
 </div>
     `);
-        const bellOptions = document.querySelector('.bell-options')
-        const startRadio = document.querySelector('#start');
-        const finishRadio = document.querySelector('#finish');
-        const bothRadio = document.querySelector('#both');
-        const silenceRadio = document.querySelector('#silence');
-
+        const bellOptions = document.querySelector('.bell-options');
+        const radioButtons = document.querySelectorAll('input[type="radio"]');
         const optionContainers = document.querySelectorAll('.option-container');
 
-
-        if (bellStart) {
-            startRadio.closest('.option-container').classList.add('option-container--active');
-        } else if (bellFinish) {
-            finishRadio.closest('.option-container').classList.add('option-container--active');
-        } else if (bellStartFinish) {
-            bothRadio.closest('.option-container').classList.add('option-container--active');
-        } else if (silence) {
-            silenceRadio.closest('.option-container').classList.add('option-container--active');
-        }
-
-
-        if (startRadio) startRadio.addEventListener('change', () => {
-            optionContainers.forEach(option => {
-                option.classList.remove('option-container--active');
-            });
-            updateBellOptions('start')
-            startRadio.closest('.option-container').classList.add('option-container--active');
-        });
-        if (finishRadio) finishRadio.addEventListener('change', () => {
-            optionContainers.forEach(option => {
-                option.classList.remove('option-container--active');
-            });
-            updateBellOptions('finish')
-            finishRadio.closest('.option-container').classList.add('option-container--active');
-        });
-        if (bothRadio) bothRadio.addEventListener('change', () => {
-            optionContainers.forEach(option => {
-                option.classList.remove('option-container--active');
-            });
-            updateBellOptions('both')
-            bothRadio.closest('.option-container').classList.add('option-container--active');
-        });
-        if (silenceRadio) silenceRadio.addEventListener('change', () => {
-            optionContainers.forEach(option => {
-                option.classList.remove('option-container--active');
-            });
-            updateBellOptions('silence')
-            silenceRadio.closest('.option-container').classList.add('option-container--active');
-
+        radioButtons.forEach(radioButton => {
+            if (bellTime == radioButton.id) {
+                radioButton.closest('.option-container').classList.add('option-container--active');
+            }
         });
 
+        radioButtons.forEach(radioButton => {
+            radioButton.addEventListener('change', () => {
+                optionContainers.forEach(option => {
+                    option.classList.remove('option-container--active');
+                });
 
+                radioButton.closest('.option-container').classList.add('option-container--active');
+                bellTime = radioButton.id;
+                console.log(bellTime);
 
+            });
+        });
 
         applyTransition(bellOptions);
-
         currentPage = 'bellConfig';
 
     }
